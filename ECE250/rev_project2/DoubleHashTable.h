b7fdf693e2e582fa098ce7b3b2f4f0cde349fe9a
OpenShift guest <cho15255@gmail.com>
2013-02-15 14:00:56 -0500

    commented working code

--- ECE250/rev_project2/DoubleHashTable.h
+++ ECE250/rev_project2/DoubleHashTable.h
@@ -59,32 +59,32 @@ occupied( new state[array_size] ) {
 
 template<typename T >
 DoubleHashTable<T >::~DoubleHashTable() {
-    
+    //delete occupied and array array
 	delete[] occupied;
     delete[] array;
 }
 
 template<typename T >
 int DoubleHashTable<T >::size() const {
-    
+    //return count (number of elements in the array)
 	return count;
 }
 
 template<typename T >
 int DoubleHashTable<T >::capacity() const {
-    
+    //return array_size (size of the array)
 	return array_size;
 }
 
 template<typename T >
 double DoubleHashTable<T >::load_factor() const {
-    
+    //returns the load factor
 	return (double)count/array_size;
 }
 
 template<typename T >
 bool DoubleHashTable<T >::empty() const {
-    
+    //returns true if the array does not have any elements inside
     if (count == 0) {
         return true;
     }
@@ -94,7 +94,7 @@ bool DoubleHashTable<T >::empty() const {
 
 template<typename T >
 int DoubleHashTable<T >::h1( T const &obj ) const {
-   
+   //calculates the probing
    int h = (int)obj&(array_size-1);
    
     while (h < 0) {
@@ -106,7 +106,7 @@ int DoubleHashTable<T >::h1( T const &obj ) const {
 
 template<typename T >
 int DoubleHashTable<T >::h2( T const &obj ) const {
-    
+    //calculates offset
     int h = (int)(obj/array_size)&(array_size-1);
     
     while (h < 0) {
@@ -122,10 +122,13 @@ int DoubleHashTable<T >::h2( T const &obj ) const {
 
 template<typename T >
 bool DoubleHashTable<T >::member( T const &obj ) const {
+    
+    //returns true if obj is inside the array
 	int probing = h1(obj);
     int offset = h2(obj);
     
     for (int i=0; i<array_size; i++) {
+        //search for the obj with appropriate hash searching (similar to inserting)
         if (array[probing] == obj && occupied[probing] == OCCUPIED) {
             return true;
         } else if (occupied[probing] == EMPTY) {
@@ -140,11 +143,13 @@ bool DoubleHashTable<T >::member( T const &obj ) const {
 
 template<typename T >
 T DoubleHashTable<T >::bin( int n ) const {
+    //returns the element in nth location of the array
 	return array[n];
 }
 
 template<typename T >
 void DoubleHashTable<T >::insert( T const &obj ) {
+    //inserts obj if there obj is not in the array
 	if (count == array_size) {
          throw overflow();
 	} else {
@@ -155,6 +160,7 @@ void DoubleHashTable<T >::insert( T const &obj ) {
         int probing = h1(obj);
         int offset = h2(obj);
         
+        //double hashing technique
         for (int i=0; i<array_size; i++) {
             if (occupied[probing] == OCCUPIED) {
                 probing += offset;
@@ -173,6 +179,7 @@ void DoubleHashTable<T >::insert( T const &obj ) {
 template<typename T >
 bool DoubleHashTable<T >::remove( T const &obj ) {
     
+    //deletes obj inside the array if it exists
     if (!member(obj)) {
         return false;
     }
@@ -180,6 +187,7 @@ bool DoubleHashTable<T >::remove( T const &obj ) {
     int probing = h1(obj);
     int offset = h2(obj);
     
+    //also uses appropriate searching technique to find obj
     while (array[probing] != obj) {
         probing = (probing + offset) & (array_size - 1);
     }
@@ -193,6 +201,8 @@ bool DoubleHashTable<T >::remove( T const &obj ) {
 
 template<typename T >
 void DoubleHashTable<T >::clear() {
+    //clears the array reinitialize all the elements in array to 0
+    //also reinitialize all the elements in occupied array to EMPTY 
     for (int i=0; i<array_size; i++) {
         array[i] = 0;
         occupied[i] = EMPTY;
@@ -203,7 +213,7 @@ void DoubleHashTable<T >::clear() {
 
 template<typename T >
 void DoubleHashTable<T >::print() const {
-    
+    //prints all the elements in the array
     for (int i=0; i<array_size; i++) {
         std::cout << array[i] << std::endl;
     } 
