4c39769cea70b633de9990dc2af14ba5fea059da
OpenShift guest <cho15255@gmail.com>
2013-02-15 01:39:58 -0500

    ha

--- ECE250/rev_project2/DoubleHashTable.h
+++ ECE250/rev_project2/DoubleHashTable.h
@@ -94,30 +94,23 @@ bool DoubleHashTable<T >::empty() const {
 
 template<typename T >
 int DoubleHashTable<T >::h1( T const &obj ) const {
-   
-   int h = static_cast<int>(obj)&(array_size-1);
-   
-    while (h < 0) {
-        return h + array_size;
-    }
-    
-	return h;
+int result = static_cast<int>(obj) & (array_size-1); //h1 i%M
+if(result < 0) //if the result is negative add M
+{ 
+    result = result + array_size;
+
+}
+	return result; 
 }
 
 template<typename T >
 int DoubleHashTable<T >::h2( T const &obj ) const {
-    
-    int h = static_cast<int>(obj)(obj/array_size)&(array_size-1);
-    
-    while (h < 0) {
-        h = h + array_size;   
-    }
-    
-    if (h%2 == 0) {
-        h += 1;
-    }
-    
-	return h;
+	int result2 = (static_cast<int>(obj/array_size) & (array_size - 1)) | 1; //h2 obj/M % M also oring with 1 to make it an odd number always
+	if(result2 < 0)
+	{
+		result2 = result2 + array_size; //if result is negative add M 
+	}
+	return result2;
 }
 
 template<typename T >
@@ -147,36 +140,27 @@ T DoubleHashTable<T >::bin( int n ) const {
 
 template<typename T >
 void DoubleHashTable<T >::insert( T const &obj ) {
-
-    if(count == array_size)
-	{
-		throw overflow();
-	}
-	else
-	{
-		if(member(obj) == false)
-		{
-			int h1status = h1(obj); //running h1 with obj
-			int h2status = h2(obj); //running h2 with obj
-
-			for(int i = 0; i < array_size; i++) //looping through the whole array
-				{
-					int position = h1status + (i*h2status); //position is equal to h1status + istatus
-					position = position & (array_size-1); //moding the position since it might go over M
-
-					if(occupied[position] == OCCUPIED) //if it is occupied then contiune the loop
-						continue;
-
-					else if(occupied[position] == EMPTY || occupied[position] == DELETED) //if the position is empty
-					{
-							array[position] = obj; //set the element to object
-							occupied[position] = OCCUPIED; //flag the position to occupied
-							count++; //increment the counter since the a new element is being added
-							break; //break out of the loop when done
-					}
-				}
-		}
-
+	if (count == array_size) {
+         throw overflow();
+	} else {
+        if (member(obj)) {
+            return;
+        }
+        
+        int probing = h1(obj);
+        int offset = h2(obj);
+        
+        for (int i=0; i<array_size; i++) {
+            if (occupied[probing] == OCCUPIED) {
+                probing = (probing + offset) & (array_size - 1);
+            } else {
+                break;
+            }
+        }
+        
+        array[probing] = obj;
+        occupied[probing] = OCCUPIED;
+        count ++;   
 	}
 }
 
